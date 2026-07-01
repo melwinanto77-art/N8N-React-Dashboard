a899ac97d1e862175bfdc4997361184796202cf0
devesh-666 <dvshrdd@gmail.com>
2026-07-01 17:33:36 +0530

    fix: resolve adjacent JSX elements syntax error in AnalyticsPanel.jsx

--- dashboard/src/components/AnalyticsPanel.jsx
+++ dashboard/src/components/AnalyticsPanel.jsx
@@ -329,6 +329,7 @@ export default function AnalyticsPanel({ site, onViewContacts }) {
                     </tbody>
                   </table>
                 </div>
+              )}
             </section>
           </div>
 
